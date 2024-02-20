cc1c31d3f0d593e9d691ff44ffc80f668313bafb
Jasper Mayone <me@jaspermayone.com>
2024-02-21 00:48:04 +0200

    resolve reese's suggustions on ActiveRecord Encryption

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,5 +1,6 @@
 class Case < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :entries
-  encrypts :name, :description, :user_id, deterministic: true
+  encrypts :user_id, deterministic: true
+  encrypts :name, :description
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true # standard:disable all
   has_secure_password
-  encrypts :email, :first_name, :last_name, deterministic: true
-  encrypts :admin, :super_admin
+  encrypts :email, deterministic: true
+  encrypts :admin, :super_admin, :first_name, :last_name
 end
